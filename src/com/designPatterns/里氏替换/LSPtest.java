6865b26002c51c0c89276f474000fa1b1887b30a
zja <zhou_ji_an@foxmail.com>
2020-05-30 14:18:08 +0800

    init

--- src/com/designPatterns/里氏替换/LSPtest.java
+++ src/com/designPatterns/里氏替换/LSPtest.java
@@ -6,7 +6,7 @@ package com.designPatterns.里氏替换;
  * 
  * 里氏替换原则通俗来讲就是：子类可以扩展父类的功能，但不能改变父类原有的功能。
  * 
- * 也就是说：子类继承父类时，除添加新的方法完成新增功能外，尽量不要重写父类的方法。
+ * 也就是说：子类继承父类时，除添加新的方法完成新增功能外，尽量不要重写父类的方法。 
  *
  * 如果通过重写父类的方法来完成新的功能，这样写起来虽然简单，但是整个继承体系的可复用性会比较差，特别是运用多态比较频繁时，程序运行出错的概率会非常大。
  *
@@ -19,13 +19,14 @@ public class LSPtest {
 		Bird swallow = new Swallow();
 		Bird brownKiwi = new BrownKiwi();
 		double distance = 300; //如果飞行距离是300
+		
 		double speed = 120;
 		
 		
 		swallow.setSpeed(speed);
 		brownKiwi.setSpeed(speed);
 		
-		System.out.println("如果飞行距离是300");
+		System.out.println("如果飞行距离是" + distance);
 		
 		try {
 			System.out.println("燕子将飞行"+swallow.getFlyTime(distance) +"小时.");
